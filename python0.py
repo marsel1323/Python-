49b7b17c61757af6b976efaa4d284b3c3bcda6cd
marsel1323 <amn132313@gmail.com>
2016-12-29 13:03:41 +0300

    Выполнен третий урок

--- python0.py
+++ python0.py
@@ -1,16 +1,37 @@
 # coding : utf-8
+import os, psutil, sys
 
-#Комментарий
+# Комментарий
+
 print("Perfect python program")
 print("Hi, fool")
 name = input("Your name:")
 
-print (name, ", Welcome to Python World!")
+print(name, ", Welcome to Python World!")
 
 answer = input ("Let's work? (Y/N)")
 
+# PEP-8
 if answer == 'Y':
-	print ("You've got moneys!")
-else:
+	print("Excelent, master")
+	print("I can:")
+	print("  [1] - List of files")
+	print("  [2] - System Information")
+	print("  [3] - List of processes")
+	
+	do = int(input("Enter the number of actions:"))
+	
+	if do == 1:
+		print(os.listdir())
+	elif do == 2:
+		pass
+	elif do == 3:
+		print(psutil.pids()
+	else:
+		pass
+		
+elif answer == 'N':
 	print("Goodbye!")
+else:
+	print("Unknown")
 	
