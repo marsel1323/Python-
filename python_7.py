aa1b783c389c443e0285450b3c09783d26e6cf7b
marsel1323 <amn132313@gmail.com>
2016-12-29 18:27:06 +0300

    закончен 8 урок

--- python_7.py
+++ python_7.py
@@ -24,6 +24,11 @@ draw_circle(5, 'red')
 phi = 360 / 7
 r = 50
 
+for i in range(0, 7):
+	phi_rad = phi * i * math.pi / 180.0
+	gotoxy(math.sin(phi_rad)*r, math.cos(phi_rad)*r + 60)
+	draw_circle(22,'white')
+
 answer = ''
 start = 0
 while answer != 'N':
@@ -44,13 +49,6 @@ while answer != 'N':
 		if start == 0:
 			gotoxy(-50,250)
 			turtle.write("You lose!", font=("Arial", 18, "normal"))
-
-		#turtle.penup()
-		#turtle.goto(random.randrange(-300,300), random.randrange(-200,200))
-		#turtle.pendown()
-		#turtle.fillcolor(random.random(),random.random(),random.random())
-		#turtle.begin_fill()
-		#turtle.circle(random.randrange(1,100))
-		#turtle.end_fill()
+		
 	else:
 		pass
